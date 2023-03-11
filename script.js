2fd789c0205e6e49a2a4c38826de6298efe87101
Rhonda Kremer <rhondakremer@gmail.com>
2023-03-10 20:36:58 -0500

    rick roll + button text

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const onImageClick = () => {
     var zplImg = document.getElementById('sad-zpl');
     var pngImg = document.getElementById('yay-png');
-    if (zplImg.style.display === 'none') {
+    var button = document.getElementById('zpl-button')
+    if (zplImg.style.display != 'block') {
         zplImg.style.display = 'block';
         pngImg.style.display = 'none';
+        button.innerHTML = 'Back to PNG';
     } else {
         zplImg.style.display = 'none';
         pngImg.style.display = 'block';
+        button.innerHTML = 'Back to ZPL';
     }
 }
